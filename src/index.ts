885eca612948e74d9064bc54532453228ba302c4
Jayson Goh Chung Sern <goh.chung.sern@gmail.com>
2025-01-24 00:10:42 +0800

    build: Update package and scope

--- src/index.ts
+++ src/index.ts
@@ -107,7 +107,7 @@ export function MicroBatcher<TParamType, TReturnType>(
       }
     }
 
-    private intercept = (func: AsyncFunction<TParamType, TReturnType>) => {
+    __intercept = (func: AsyncFunction<TParamType, TReturnType>) => {
       const runBatcher = (processCount?: number) => {
         MicroBatcherBuilder._currentBatchTimeoutId = undefined;
         // TODO: add concurrent batcher limit support
@@ -175,7 +175,7 @@ export function MicroBatcher<TParamType, TReturnType>(
     };
 
     build(): AsyncFunction<TParamType, TReturnType> {
-      return this.intercept(MicroBatcherBuilder._singlePayloadFunction);
+      return this.__intercept(MicroBatcherBuilder._singlePayloadFunction);
     }
   }
 
